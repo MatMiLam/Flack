8fb63eb8bd2cfd112940d105df1c76f1fd083f5c
MatMiLam <Mat.Lamoureux@gmail.com>
2019-09-25 22:27:39 -0500

    Sidebar Room html

--- application.py
+++ application.py
@@ -1,14 +1,35 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, request, redirect
 from flask_socketio import SocketIO, emit
+from flask_session import Session
+
+from helpers import login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SESSION_TYPE"] = "filesystem"
 socketio = SocketIO(app)
-FLASK_DEBUG=1
+Session(app)
+
+chatRooms = ["General", "News", "Sports", "Tech"]
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""  
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        session["user_id"] = username
+        return redirect("/")       
+
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@login_required
+def chat():
+    return render_template("chat.html", chatRooms=chatRooms)
+
