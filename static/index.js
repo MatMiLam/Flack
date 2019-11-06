9e6aa7264f7dd3d8877457a25fb4b6200bc72e29
MatMiLam <Mat.Lamoureux@gmail.com>
2019-11-06 11:16:40 -0600

    Troubleshooting socket io error

--- static/index.js
+++ static/index.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
         
     // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);    
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port, {
+        transports: ['polling']
+     });    
 
     //////////////////////// New Room /////////////////////////////////////////////
 
