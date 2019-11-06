2c1bab4e187664ceeb69e53bc40d809b0aa3feb6
MatMiLam <Mat.Lamoureux@gmail.com>
2019-11-06 16:23:50 -0600

    Troubleshooting socket io

--- static/index.js
+++ static/index.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
         
     // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);    
-    console.log(`Port: ${location.port}`)  
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);        
 
     //////////////////////// New Room /////////////////////////////////////////////
 
