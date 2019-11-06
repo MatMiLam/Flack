7213c1794585e89f3d3f4a9cd947b73d2fd7970a
MatMiLam <Mat.Lamoureux@gmail.com>
2019-11-06 15:22:32 -0600

    Troubleshooting socket io error

--- static/index.js
+++ static/index.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
         
     // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);    
+    // var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);    
+    var socket = io.connect(https: + '//' + matmilam-flack.herokuapp.com/ + ':' + location.port);    
 
     //////////////////////// New Room /////////////////////////////////////////////
 
